e0ee0eb7448cdfad984825a14193737e9fd69e28
Samuel-Felix <129121031+Samuel-Felix@users.noreply.github.com>
2024-02-21 15:54:49 -0300

    Delete python-aula-2.py

--- python-aula-2.py
+++
@@ -1,7 +0,0 @@
-#Programa para Calcular IMC
-#Samuel 05/02/2024
-
-print('**********************************')
-print('*******  CALCULADORA DE IMC  *****')
-print('**********************************')
-
